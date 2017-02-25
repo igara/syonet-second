c8a5fe0a3bc830b696b936d5ae95e08e5f9ab298
五十嵐翔 <igara1119@gmail.com>
2017-02-25 13:17:17 +0900

    不要なモジュールの削除

--- imports/startup/server/fixtures.js
+++
@@ -1,7 +0,0 @@
-Meteor.startup(() => {
-  WebApp.connectHandlers.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentaial", true);
-    return next();
-  });
-});
